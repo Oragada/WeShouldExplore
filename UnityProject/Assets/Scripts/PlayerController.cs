ada00a9e991ab5f551a5e2ab5120737a3cc1dc57
M1chbeck <Mr_B.K@web.de>
2013-10-19 14:15:46 +0200

    Added Sit(space) & Interaction(e) Keys
    also alternate walking with holding shift for the player.

--- UnityProject/Assets/Scripts/PlayerController.cs
+++ UnityProject/Assets/Scripts/PlayerController.cs
@@ -10,17 +10,60 @@ public enum Direction {North,South,East,West,None};
 
 public class PlayerController : MonoBehaviour {
 	
+	//publics
 	public float speed;
 	public ActiveTile actTile;
+	//privates
+	private bool isSitting=false;
+	private bool isInteracting=false;
+	private SphereCollider thisCollider = null;
+	
+	//get the collider component once, because the GetComponent-call is expansive
+	void Awake()
+	{
+		thisCollider = this.GetComponent<SphereCollider>();		
+		//rigidbody.freezeRotation = true;//would push the sphere over the landscape
+	}
 	
 	// Update is called once per frame before the physics are computed
 	void FixedUpdate () 
 	{
-        float moveHorizontal = Input.GetAxis("Horizontal");
-		float moveVertical = Input.GetAxis("Vertical");
+		// Cache the inputs.
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+        bool sit = Input.GetButtonDown("Sit");
+		bool interact = Input.GetButtonDown("Interact");
 		
-		Vector3 movement = new Vector3(moveHorizontal,0.0f,moveVertical);
-		rigidbody.AddForce (movement*speed*Time.deltaTime);
+		if( sit )
+		{
+			if(!isSitting) 
+			{
+				//stop the motion
+				rigidbody.velocity = Vector3.zero;
+				rigidbody.angularVelocity = Vector3.zero;		
+				//sit down
+				thisCollider.radius = 0.2f; 
+				isSitting = true;
+			}
+			else
+			{
+				//stand up again
+				thisCollider.radius = 0.5f;
+				isSitting = false;
+			}
+		}	
+		
+        Vector3 movement = new Vector3(h,0.0f,v);
+		if( Input.GetButton("AlternateMovement"))
+			movement= new Vector3(h+v,0.0f,v-h);
+		
+		if ( !isSitting)
+		{
+			rigidbody.AddForce (movement*speed*Time.deltaTime);
+		}
+		//trying out different stuffs // doesnt really work
+		//transform.position = speed * moveHorizontal * Time.deltaTime;
+		//rigidbody.MovePosition(movement * Time.deltaTime * speed);		
 	}
 	
 	void OnTriggerEnter (Collider other)
