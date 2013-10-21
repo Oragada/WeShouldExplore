85968da5de3134d5724fdc8d41975bee4a9231c4
M1chbeck <Mr_B.K@web.de>
2013-10-21 22:53:51 +0200

    Moved away from old Force-based Movement
    implemented 3 different input methods. toggle via "m" or "slider". My
    fav speed is 50 and inputstyle all the way to the right.

--- UnityProject/Assets/Scripts/PlayerController.cs
+++ UnityProject/Assets/Scripts/PlayerController.cs
@@ -16,21 +16,23 @@ public class PlayerController : MonoBehaviour {
 	//privates
 	private bool isSitting=false;
 	private bool isInteracting=false;
-	private SphereCollider thisCollider = null;
+	private int movementMode = 0;
 	
 	//get the collider component once, because the GetComponent-call is expansive
 	void Awake()
 	{
-		thisCollider = this.GetComponent<SphereCollider>();		
 		//rigidbody.freezeRotation = true;//would push the sphere over the landscape
-	}
+	}	
 	
-	// Update is called once per frame before the physics are computed
-	void FixedUpdate () 
+	
+	void Update () 
 	{
 		// Cache the inputs.
         float h = Input.GetAxis("Horizontal");
         float v = Input.GetAxis("Vertical");
+		// changed the Axis gravity&sensitivity to 1000, for more direct input.
+		// for joystick usage however Vince told me to:
+		/* just duplicate Horizontal and use gravity 1, dead 0.2 and sensitivity 1 that it works*/		
         bool sit = Input.GetButtonDown("Sit");
 		bool interact = Input.GetButtonDown("Interact");
 		
@@ -39,31 +41,98 @@ public class PlayerController : MonoBehaviour {
 			if(!isSitting) 
 			{
 				//stop the motion
-				rigidbody.velocity = Vector3.zero;
-				rigidbody.angularVelocity = Vector3.zero;		
+				//rigidbody.velocity = Vector3.zero;
+				//rigidbody.angularVelocity = Vector3.zero;		
 				//sit down
-				thisCollider.radius = 0.2f; 
+				gameObject.transform.localScale = new Vector3(1.0f,0.5f,1.0f);				
+				gameObject.transform.Translate(new Vector3(0.0f,-0.25f,0.0f));
 				isSitting = true;
 			}
 			else
 			{
 				//stand up again
-				thisCollider.radius = 0.5f;
+				gameObject.transform.localScale = new Vector3(1.0f,1.0f,1.0f);
+				gameObject.transform.Translate(new Vector3(0.0f,0.25f,0.0f));
 				isSitting = false;
 			}
 		}	
 		
-        Vector3 movement = new Vector3(h,0.0f,v);
-		if( Input.GetButton("AlternateMovement"))
-			movement= new Vector3(h+v,0.0f,v-h);
+        if( Input.GetButtonDown("ToggleMovementMode"))
+		{	movementMode++;
+			if( movementMode > 2)
+				movementMode = 0;
+		}
 		
 		if ( !isSitting)
 		{
-			rigidbody.AddForce (movement*speed*Time.deltaTime);
-		}
-		//trying out different stuffs // doesnt really work
-		//transform.position = speed * moveHorizontal * Time.deltaTime;
-		//rigidbody.MovePosition(movement * Time.deltaTime * speed);		
+			if( movementMode == 0) // DPAD mode
+			{
+				if( v > 0.05f )
+					gameObject.transform.Translate(new Vector3(0.1f,0.0f,0.0f)*Time.deltaTime*speed);
+				if( v < -0.05f )
+					gameObject.transform.Translate(new Vector3(-0.1f,0.0f,0.0f)*Time.deltaTime*speed);
+				if( h < -0.05f )
+					gameObject.transform.Translate(new Vector3(0.0f,0.0f,0.1f)*Time.deltaTime*speed);
+				if( h > 0.05f )
+					gameObject.transform.Translate(new Vector3(0.0f,0.0f,-0.1f)*Time.deltaTime*speed);
+			}
+			else if( movementMode == 1) // diagonal mode version one
+			{
+				Vector3 move = new Vector3(0.0f,0.0f,0.0f);
+				if( v > 0 )
+				{
+					move.x += 0.1f;
+					move.z += 0.1f;
+				}
+				if( v < 0 )
+				{
+					move.x -= 0.1f;
+					move.z -= 0.1f;
+				}
+				if( h < 0 )
+				{
+					move.x -= 0.1f;
+					move.z += 0.1f;
+				}
+				if( h > 0 )
+				{
+					move.x += 0.1f;
+					move.z -= 0.1f;
+				}
+				gameObject.transform.Translate(move*Time.deltaTime*speed);
+			}
+			else if( movementMode == 2) // diagonal mode 
+			{
+				Vector3 move = new Vector3(0.0f,0.0f,0.0f);
+				if( v > 0 )
+				{
+					move.x = Mathf.Min(0.1f, move.x+0.1f);
+					move.z = Mathf.Min(0.1f, move.z+0.1f);
+				}
+				if( v < 0 )
+				{
+					move.x =  Mathf.Max(-0.1f, move.x-0.1f);
+					move.z =  Mathf.Max(-0.1f, move.z-0.1f);					
+				}
+				if( h < 0 )
+				{
+					move.x =  Mathf.Max(-0.1f, move.x-0.1f);
+					move.z = Mathf.Min(0.1f, move.z+0.1f);
+				}
+				if( h > 0 )
+				{
+					move.x = Mathf.Min(0.1f, move.x+0.1f);
+					move.z =  Mathf.Max(-0.1f, move.z-0.1f);					
+				}
+				// when moving diagnoal reduce walk speed by sqrt(2)
+				if( move.x != 0.0f && move.z != 0.0f)
+				{
+					move.x /= Mathf.Sqrt( 2.0f );
+					move.z /= Mathf.Sqrt( 2.0f );
+				}
+				gameObject.transform.Translate(move*Time.deltaTime*speed);
+			}
+		}	
 	}
 	
 	void OnTriggerEnter (Collider other)
@@ -79,22 +148,22 @@ public class PlayerController : MonoBehaviour {
 			if( other.name == "WestTrigger")
 			{
 				dir = Direction.West;
-				rigidbody.MovePosition(new Vector3(-(other.gameObject.transform.position.x+2.0f),rigidbody.position.y,rigidbody.position.z)); // move to east 
+				gameObject.transform.Translate(new Vector3((-other.gameObject.transform.position.x*2)-2.0f,0.0f,0.0f),Space.World); // move to east 
 			}
 			else if( other.name == "EastTrigger")
 			{
 				dir = Direction.East;
-				rigidbody.MovePosition(new Vector3(-(other.gameObject.transform.position.x-2.0f),rigidbody.position.y,rigidbody.position.z)); // move to west 
+				gameObject.transform.Translate(new Vector3(-(other.gameObject.transform.position.x*2)+2.0f,0.0f,0.0f),Space.World); // move to west 
 			}
 			else if( other.name == "NorthTrigger")
 			{
 				dir = Direction.North;
-				rigidbody.MovePosition(new Vector3(rigidbody.position.x,rigidbody.position.y,-(other.gameObject.transform.position.z-2.0f))); // move to south 
+				gameObject.transform.Translate(new Vector3(0.0f, 0.0f, -(other.gameObject.transform.position.z*2)+2.0f),Space.World); // move to south 
 			}
 			else if( other.name == "SouthTrigger")
 			{
 				dir = Direction.South;
-				rigidbody.MovePosition(new Vector3(rigidbody.position.x,rigidbody.position.y,-(other.gameObject.transform.position.z+2.0f))); // move to north 
+				gameObject.transform.Translate(new Vector3(0.0f, 0.0f, (-other.gameObject.transform.position.z*2)-2.0f),Space.World); // move to south 
 			}
 			
 			// update tile, pass the direction along
@@ -103,7 +172,17 @@ public class PlayerController : MonoBehaviour {
 	}
     void OnGUI()
     {
-        speed = GUI.HorizontalSlider(new Rect(25, 35, 300, 30), speed, 0f, 2000.0f);
+        speed = GUI.HorizontalSlider(new Rect(25, 35, 300, 10), speed, 0f, 500.0f);
         GUI.Label(new Rect(25,15,60,20), speed.ToString(CultureInfo.InvariantCulture));
+		float test = GUI.HorizontalSlider(new Rect(25, 65, 50, 10), movementMode, 0.0f, 2.0f);
+		GUI.Label(new Rect(25,45,150,20), "AlternateMoveStyle:");
+		if( test > 1.5f) 
+			movementMode = 2;
+		else if( test > 0.5f)
+			movementMode = 1;
+		else
+			movementMode = 0;
+			
+      //  GUI.Label(new Rect(25,15,60,20), movementMode.ToString(CultureInfo.InvariantCulture));
     }
 }
